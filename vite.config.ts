275dc5cbaca9047b1b0c92d50abf43c70c163648
kamilgaraev <kamilgaraev11@gmail.com>
2025-07-18 02:24:03 +0300

    Добавлен путь к модулю react-router в конфигурацию Vite для улучшения маршрутизации в проекте. Эти изменения направлены на оптимизацию функциональности и поддержку актуальности используемых библиотек.

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@utils': path.resolve(__dirname, './src/utils'),
       '@renderer': path.resolve(__dirname, './src/renderer'),
+      'react-router': path.resolve(__dirname, './node_modules/react-router/index.js'),
     },
   },
 }); 
